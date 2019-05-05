df4fe6d09d1fc0466ad6c0fdb659b3634b47bee7
David Lorenzo <17401854+David-Lor@users.noreply.github.com>
2019-05-05 17:54:20 +0200

    Add files via upload

--- PowerOutageDetector_Simple/PowerOutageDetector_Simple.ino
+++ PowerOutageDetector_Simple/PowerOutageDetector_Simple.ino
@@ -1,1 +1,67 @@
+/*  ARDUINO POWER OUTAGE DETECTOR
+ *  
+ *  - Detect power outage on the main power line using an external DC input and an optocoupler.
+ *  - Notify through Serial.
+ *  - Turn ON/OFF two leds depending on power status.
+ */
 
+//Pinout & loop
+#define PIN_INPUT 2
+#define PIN_LED_ON 3
+#define PIN_LED_OFF 4
+#define LOOP_DELAY 100
+
+//Serial settings & messages
+#define SERIAL_SPEED 9600
+#define SERIAL_MSG_STARTUP "PowerOutageDetector:ONLINE"
+#define SERIAL_STATUS_ON "Power:ON"
+#define SERIAL_STATUS_OFF "Power:OFF"
+
+//Global variables
+byte powerStatus = 255;
+
+////////////////////////////////////////////////
+
+void setLED() {
+  digitalWrite(PIN_LED_ON, powerStatus);
+  digitalWrite(PIN_LED_OFF, !powerStatus);
+}
+
+bool readStatus() {
+  return digitalRead(PIN_INPUT);
+}
+
+void sendStatus() {
+  if (powerStatus) {
+    Serial.println(SERIAL_STATUS_ON);
+  } else {
+    Serial.println(SERIAL_STATUS_OFF);
+  }
+}
+
+////////////////////////////////////////////////
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(PIN_INPUT, INPUT);
+  pinMode(PIN_LED_ON, OUTPUT);
+  pinMode(PIN_LED_OFF, OUTPUT);
+  Serial.println(F(SERIAL_MSG_STARTUP));
+
+  //Disable peripherals
+  ADCSRA = ADCSRA & B01111111;
+  ACSR = B10000000;
+  DIDR0 = DIDR0 | B00111111;
+}
+
+void loop() {
+  bool currentStatus = readStatus();
+  
+  if (currentStatus != powerStatus) {
+    powerStatus = currentStatus;
+    setLED();
+    sendStatus();
+  }
+
+  delay(LOOP_DELAY);
+}
